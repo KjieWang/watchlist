a60f7dbc790853ce0573eb0159ae2113a1d673c7
wkj <2672126643@qq.com>
2022-01-10 00:50:02 +0800

    对于templates的学习

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
-from flask import escape,url_for
-app = Flask(__name__)
+from flask import escape,url_for,render_template
+app=Flask(__name__)
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
-@app.route('/hello')
-@app.route('/')
 @app.route('/home')
-
 def hello():
 	return '<h1>欢迎学习flask框架</h1>'
 
@@ -24,3 +34,6 @@ def test_url_for():
 	pirnt(url_for('user_page',name='pangpang'))
 	print(url_for('test_url_for',min=2))
 	return 'Test Page'
+@app.route('/')
+def index():
+	return render_template("index.html",name=name,movies=movies)
