90d636939bdd386705f28c68085420ff894ddd94
wkj <2672126643@qq.com>
2022-01-11 22:50:19 +0800

    创建了表单，删除以及编辑按钮及其功能实现

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import click
 from flask import Flask
-from flask import escape,url_for,render_template
+from flask import escape,url_for,render_template,flash,request
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -52,11 +52,22 @@ def initdb(drop):
 	db.create_all()
 	click.echo("初始化数据库")
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-	user = User.query.first()
+	if request.method == 'POST':
+		title = request.form.get('title')
+		year = request.form.get('year')
+		if not title or not year or len(year) > 4 or len(title) > 60:
+			flash('Invaild input.')
+			return redirect(url_for('index'))
+
+		movie = Movie(title=title,year=year)
+		db.session.add(movie)
+		db.session.commit()
+		flash('Item created.')
+		return redirect(url_for('index'))
 	movies = Movie.query.all()
-	return render_template('index.html',user=user,movies=movies)
+	return render_template('index.html',movies=movies)
 
 @app.cli.command()
 def forge():
@@ -81,3 +92,37 @@ def forge():
 		db.session.add(movie)
 	db.session.commit()
 	click.echo('插入数据到数据库')
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return (render_template('404.html'),404)
+@app.context_processor
+def inject_user():
+	user = User.query.first()
+	return dict(user=user)
+
+@app.route('/movie/edit/<int:movie_id>',methods=['GET','POST'])
+def edit(movie_id):
+	movie = Movie.query.get_or_404(movie_id)
+	if request.method == 'POST':
+		title = request.form['title']
+		year = request.form['year']
+
+		if not title or not year or len(year) != 4 or len(title) > 60:
+			flash('Invaild input.')
+			return redirect(url_for('edit',movie_id=movie_id))
+
+		movie.title = title
+		movie.year = year
+		db.session.commit()
+		flash('Item updated.')
+		return redirect(url_for('index'))
+	return render_template('edit.html',movie=movie)
+
+@app.route('/movie/delete/<int:movie_id>',methods=['POST'])
+def delete(movie_id):
+	movie=Movie.query.get_or_404(movie_id)
+	db.session.delete(movie)
+	db.session.commit()
+	flash('Item deleted.')
+	return redirect(url_for('index'))
